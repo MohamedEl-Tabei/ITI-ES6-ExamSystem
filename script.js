a39d91ce60befc0d62ee9b890655f9f196e5fef2
MohamedEl-Tabei <mohamedeltabei2000@gmail.com>
2025-01-25 20:41:24 +0200

    finish login, ready to start and solve the bug of events

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+import goTo from "./utilities/goTo.js";
 import loadPage from "./utilities/loadPage.js";
-
+import Signup from "./authentication/signup.js";
+import Login from "./authentication/login.js";
 (async () => {
-  await loadPage(sessionStorage.getItem("viewName") || "_signup");
+  await loadPage("_signup");
+  await loadPage("_login");
+  await loadPage("_ready");
+  goTo(sessionStorage.getItem("page") || "_signup");
+  // to add events after load the pages
+  Signup();
+  Login();
 })();
