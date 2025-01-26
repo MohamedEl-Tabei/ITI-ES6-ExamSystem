6b760ca771981e902bd4754ea115f0ba6c45de5b
Mohamed <robinvanhood20@gmail.com>
2025-01-26 20:45:33 +0200

    Fail page

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ import loadPage from "./utilities/loadPage.js";
 import Signup from "./authentication/signup.js";
 import Login from "./authentication/login.js";
 import Success from "./controllers/success.js";
+import Fail from "./controllers/fail.js";
+
 (async () => {
   await loadPage("_signup");
   await loadPage("_login");
   await loadPage("_ready");
   await loadPage("_success");
+  await loadPage("_fail");
   goTo(sessionStorage.getItem("page") || "_signup");
   // to add events after load the pages
   Signup();
   Login();
   Success();
+  Fail();
 })();
